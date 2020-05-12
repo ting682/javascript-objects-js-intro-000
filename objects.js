b79dc5f15407fa9c28b01571d5a01729196a3933
Ting Chung <tchung682@gmail.com>
2020-05-12 14:48:01 +0000

    Automatically backed up by Learn

--- objects.js
+++ objects.js
@@ -7,6 +7,6 @@ function updatePlaylist(playlist, artistName, songTitle){
 }
 
 function removeFromPlaylist(playlist, artistName){
-  Delete playlist.artistName;
+  delete playlist.artistName;
   return playlist;
 }
